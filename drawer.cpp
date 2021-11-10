99064625c9b0a76c25f1a903e15db559ec20dad8
Andriy Semenistiy <46621668+MeowningMaster@users.noreply.github.com>
2021-11-10 13:41:54 +0200

    add axes, scale

--- drawer.cpp
+++ drawer.cpp
@@ -27,6 +27,7 @@ void Drawer::draw(){
     {
         int width = storedSize.width(), height = storedSize.height();
         int halfWidth = width / 2;
+        int halfHeight = height / 2;
         qreal scale = storedScale;
         qreal startPosX = (-storedOffset.x() - halfWidth) * scale, posX;
         int normalizedOffsetY = height / 2 + storedOffset.y();
@@ -41,7 +42,32 @@ void Drawer::draw(){
         expression.register_symbol_table(symbolTable);
         exprtk::parser<qreal> parser;
 
-        auto formulas = QVector<QString>(storedFormulas);
+        // draw axes
+        QPoint center(halfWidth + storedOffset.x(), halfHeight + storedOffset.y());
+        painter.drawLine(0, center.y(), width, center.y()); // x
+        painter.drawLine(center.x(), 0, center.x(), height); // y
+
+        // draw marks
+        int tickHalf = 5;
+        int distance = 20;
+        for (int i = center.y() - distance; i >= 0; i -= distance) {
+            painter.drawLine(center.x() - tickHalf, i, center.x() + tickHalf, i);
+        }
+        for (int i = center.y() + distance; i < height; i += distance) {
+            painter.drawLine(center.x() - tickHalf, i, center.x() + tickHalf, i);
+        }
+        for (int i = center.x() - distance; i >= 0; i -= distance) {
+            painter.drawLine(i, center.y() - tickHalf, i, center.y() + tickHalf);
+        }
+        for (int i = center.x() + distance; i < width; i += distance) {
+            painter.drawLine(i, center.y() - tickHalf, i, center.y() + tickHalf);
+        }
+
+        // draw scale
+        painter.drawText(QPoint(20, height - 20), QString::number(distance * scale));
+
+        // draw graphs
+        auto formulas = QStringList(storedFormulas);
         for (QString& formula : formulas) {
             parser.compile(formula.toStdString(), expression);
             posX = startPosX;
